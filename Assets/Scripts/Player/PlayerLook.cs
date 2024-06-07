efb5e9b948edf4c56ee5ab06c9cd0f1de66f566b
James Anderson <j.anderson2429@gmail.com>
2024-06-07 15:02:15 -0400

    added grid snapping to the player look indicator

--- Assets/Scripts/Player/PlayerLook.cs
+++ Assets/Scripts/Player/PlayerLook.cs
@@ -16,6 +16,10 @@ public class PlayerLook : MonoBehaviour {
     [Header("Layer Settings")]
     public LayerMask groundLayer;
 
+    [Header("Grid Settings")]
+    public float cellSize;
+    public Vector2 gridOffset;
+
     private void Start() {
         if (playerLookIndicatorPrefab == null) {
             Debug.LogError("Player Look Indicator Prefab is not set!");
@@ -41,10 +45,24 @@ public class PlayerLook : MonoBehaviour {
                 playerLookIndicator.SetActive(true);
             }
 
-            playerLookIndicator.transform.position = _hit.point;
+            Vector3 hitPoint = _hit.point;
+            playerLookIndicator.transform.position = ClampToNearest(hitPoint, cellSize);
             Debug.Log("Player Look Indicator Position: " + playerLookIndicator.transform.position);
         } else {
             Debug.Log("Raycast did not hit the ground layer!");
         }
+    }
+
+
+    private Vector3 ClampToNearest(Vector3 position, float threshold) {
+        float t = 1f / threshold;
+        Vector3 v = ((Vector3)Vector3Int.FloorToInt(position * t)) / t;
+
+        // Offset to center of cell
+        float s = threshold / 2.0f;
+        v.x += s + gridOffset.x;
+        v.z += s + gridOffset.y;
+
+        return v;
     }
 }
